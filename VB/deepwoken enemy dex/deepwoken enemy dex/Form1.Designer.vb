7876d482414de961e3b07684649273862bd7c32e
Chandler Mantione <Cdman806@learner.dvusd.org>
2022-08-17 14:06:03 -0700

    Wednesday
    August, 17 2022

---
+++ VB/deepwoken enemy dex/deepwoken enemy dex/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ThresherButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.DifficultyLabel = New System.Windows.Forms.Label()
+        Me.MovesLabel = New System.Windows.Forms.Label()
+        Me.TypeOfEnemyLabel = New System.Windows.Forms.Label()
+        Me.FairyManButton = New System.Windows.Forms.Button()
+        Me.SharkoButton = New System.Windows.Forms.Button()
+        Me.DeepOwlButton = New System.Windows.Forms.Button()
+        Me.PrimadonButton = New System.Windows.Forms.Button()
+        Me.PrimadonPictureBox = New System.Windows.Forms.PictureBox()
+        Me.DeepOwlPictureBox = New System.Windows.Forms.PictureBox()
+        Me.SharkoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.FairyManPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ThresherPictureBox = New System.Windows.Forms.PictureBox()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.PrimadonPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DeepOwlPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SharkoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FairyManPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ThresherPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ThresherButton
+        '
+        Me.ThresherButton.Location = New System.Drawing.Point(21, 313)
+        Me.ThresherButton.Name = "ThresherButton"
+        Me.ThresherButton.Size = New System.Drawing.Size(81, 23)
+        Me.ThresherButton.TabIndex = 0
+        Me.ThresherButton.Text = "Thresher"
+        Me.ThresherButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(323, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(323, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Difficulty:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(323, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Moves:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(327, 162)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "type of enemy:"
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(364, 9)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(0, 16)
+        Me.NameLabel.TabIndex = 7
+        '
+        'DifficultyLabel
+        '
+        Me.DifficultyLabel.AutoSize = True
+        Me.DifficultyLabel.Location = New System.Drawing.Point(380, 25)
+        Me.DifficultyLabel.Name = "DifficultyLabel"
+        Me.DifficultyLabel.Size = New System.Drawing.Size(0, 16)
+        Me.DifficultyLabel.TabIndex = 8
+        '
+        'MovesLabel
+        '
+        Me.MovesLabel.AutoSize = True
+        Me.MovesLabel.Location = New System.Drawing.Point(373, 41)
+        Me.MovesLabel.Name = "MovesLabel"
+        Me.MovesLabel.Size = New System.Drawing.Size(0, 16)
+        Me.MovesLabel.TabIndex = 9
+        '
+        'TypeOfEnemyLabel
+        '
+        Me.TypeOfEnemyLabel.AutoSize = True
+        Me.TypeOfEnemyLabel.Location = New System.Drawing.Point(427, 162)
+        Me.TypeOfEnemyLabel.Name = "TypeOfEnemyLabel"
+        Me.TypeOfEnemyLabel.Size = New System.Drawing.Size(0, 16)
+        Me.TypeOfEnemyLabel.TabIndex = 10
+        '
+        'FairyManButton
+        '
+        Me.FairyManButton.Location = New System.Drawing.Point(152, 313)
+        Me.FairyManButton.Name = "FairyManButton"
+        Me.FairyManButton.Size = New System.Drawing.Size(81, 23)
+        Me.FairyManButton.TabIndex = 11
+        Me.FairyManButton.Text = "Fairy Man"
+        Me.FairyManButton.UseVisualStyleBackColor = True
+        '
+        'SharkoButton
+        '
+        Me.SharkoButton.Location = New System.Drawing.Point(295, 313)
+        Me.SharkoButton.Name = "SharkoButton"
+        Me.SharkoButton.Size = New System.Drawing.Size(75, 23)
+        Me.SharkoButton.TabIndex = 13
+        Me.SharkoButton.Text = "Sharko"
+        Me.SharkoButton.UseVisualStyleBackColor = True
+        '
+        'DeepOwlButton
+        '
+        Me.DeepOwlButton.Location = New System.Drawing.Point(430, 313)
+        Me.DeepOwlButton.Name = "DeepOwlButton"
+        Me.DeepOwlButton.Size = New System.Drawing.Size(101, 23)
+        Me.DeepOwlButton.TabIndex = 15
+        Me.DeepOwlButton.Text = "Deep Owl"
+        Me.DeepOwlButton.UseVisualStyleBackColor = True
+        '
+        'PrimadonButton
+        '
+        Me.PrimadonButton.Location = New System.Drawing.Point(583, 313)
+        Me.PrimadonButton.Name = "PrimadonButton"
+        Me.PrimadonButton.Size = New System.Drawing.Size(100, 23)
+        Me.PrimadonButton.TabIndex = 17
+        Me.PrimadonButton.Text = "Primadon"
+        Me.PrimadonButton.UseVisualStyleBackColor = True
+        '
+        'PrimadonPictureBox
+        '
+        Me.PrimadonPictureBox.Image = Global.deepwoken_enemy_dex.My.Resources.Resources.Primadon
+        Me.PrimadonPictureBox.Location = New System.Drawing.Point(558, 342)
+        Me.PrimadonPictureBox.Name = "PrimadonPictureBox"
+        Me.PrimadonPictureBox.Size = New System.Drawing.Size(145, 96)
+        Me.PrimadonPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PrimadonPictureBox.TabIndex = 18
+        Me.PrimadonPictureBox.TabStop = False
+        '
+        'DeepOwlPictureBox
+        '
+        Me.DeepOwlPictureBox.Image = Global.deepwoken_enemy_dex.My.Resources.Resources.Owl
+        Me.DeepOwlPictureBox.Location = New System.Drawing.Point(411, 342)
+        Me.DeepOwlPictureBox.Name = "DeepOwlPictureBox"
+        Me.DeepOwlPictureBox.Size = New System.Drawing.Size(141, 96)
+        Me.DeepOwlPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.DeepOwlPictureBox.TabIndex = 16
+        Me.DeepOwlPictureBox.TabStop = False
+        '
+        'SharkoPictureBox
+        '
+        Me.SharkoPictureBox.Image = Global.deepwoken_enemy_dex.My.Resources.Resources.Sharko
+        Me.SharkoPictureBox.Location = New System.Drawing.Point(264, 342)
+        Me.SharkoPictureBox.Name = "SharkoPictureBox"
+        Me.SharkoPictureBox.Size = New System.Drawing.Size(141, 96)
+        Me.SharkoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.SharkoPictureBox.TabIndex = 14
+        Me.SharkoPictureBox.TabStop = False
+        '
+        'FairyManPictureBox
+        '
+        Me.FairyManPictureBox.Image = Global.deepwoken_enemy_dex.My.Resources.Resources.Fairy_man
+        Me.FairyManPictureBox.Location = New System.Drawing.Point(133, 342)
+        Me.FairyManPictureBox.Name = "FairyManPictureBox"
+        Me.FairyManPictureBox.Size = New System.Drawing.Size(125, 96)
+        Me.FairyManPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.FairyManPictureBox.TabIndex = 12
+        Me.FairyManPictureBox.TabStop = False
+        '
+        'ThresherPictureBox
+        '
+        Me.ThresherPictureBox.Image = Global.deepwoken_enemy_dex.My.Resources.Resources.Thresher
+        Me.ThresherPictureBox.Location = New System.Drawing.Point(2, 342)
+        Me.ThresherPictureBox.Name = "ThresherPictureBox"
+        Me.ThresherPictureBox.Size = New System.Drawing.Size(125, 96)
+        Me.ThresherPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ThresherPictureBox.TabIndex = 2
+        Me.ThresherPictureBox.TabStop = False
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(2, 0)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(319, 178)
+        Me.DisplayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.DisplayPictureBox.TabIndex = 1
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PrimadonPictureBox)
+        Me.Controls.Add(Me.PrimadonButton)
+        Me.Controls.Add(Me.DeepOwlPictureBox)
+        Me.Controls.Add(Me.DeepOwlButton)
+        Me.Controls.Add(Me.SharkoPictureBox)
+        Me.Controls.Add(Me.SharkoButton)
+        Me.Controls.Add(Me.FairyManPictureBox)
+        Me.Controls.Add(Me.FairyManButton)
+        Me.Controls.Add(Me.TypeOfEnemyLabel)
+        Me.Controls.Add(Me.MovesLabel)
+        Me.Controls.Add(Me.DifficultyLabel)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ThresherPictureBox)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Controls.Add(Me.ThresherButton)
+        Me.Name = "Form1"
+        Me.Text = "deepwoken enemy dex"
+        CType(Me.PrimadonPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DeepOwlPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SharkoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FairyManPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ThresherPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ThresherButton As Button
+    Friend WithEvents DisplayPictureBox As PictureBox
+    Friend WithEvents ThresherPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents DifficultyLabel As Label
+    Friend WithEvents MovesLabel As Label
+    Friend WithEvents TypeOfEnemyLabel As Label
+    Friend WithEvents FairyManPictureBox As PictureBox
+    Friend WithEvents FairyManButton As Button
+    Friend WithEvents SharkoButton As Button
+    Friend WithEvents SharkoPictureBox As PictureBox
+    Friend WithEvents DeepOwlButton As Button
+    Friend WithEvents DeepOwlPictureBox As PictureBox
+    Friend WithEvents PrimadonButton As Button
+    Friend WithEvents PrimadonPictureBox As PictureBox
+End Class
---
+++ VB/deepwoken enemy dex/deepwoken enemy dex/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Private Sub ThresherButton_Click(sender As Object, e As EventArgs) Handles ThresherButton.Click
+        DisplayPictureBox.Image = ThresherPictureBox.Image
+        NameLabel.Text = "Thresher"
+        DifficultyLabel.Text = "Easy"
+        MovesLabel.Text = "Arm Swipe
+Bite
+Tail Swipe
+Burrow"
+        TypeOfEnemyLabel.Text = "Monster"
+    End Sub
+
+    Private Sub FairyManButton_Click(sender As Object, e As EventArgs) Handles FairyManButton.Click
+        DisplayPictureBox.Image = FairyManPictureBox.Image
+        NameLabel.Text = "Fairy Man"
+        DifficultyLabel.Text = "Medium-Hard"
+        MovesLabel.Text = "Displacement
+Lightning Clones
+Lightning Strike
+Lightning Assault
+Electromagnetic Pulse
+Grand Volley"
+        TypeOfEnemyLabel.Text = "Boss Human"
+    End Sub
+
+    Private Sub SharkoButton_Click(sender As Object, e As EventArgs) Handles SharkoButton.Click
+        DisplayPictureBox.Image = SharkoPictureBox.Image
+        NameLabel.Text = "Sharko"
+        DifficultyLabel.Text = "Easy"
+        MovesLabel.Text = "Slash
+Kick
+Jump
+Thousand Needles
+Roar"
+        TypeOfEnemyLabel.Text = "Monster"
+    End Sub
+
+    Private Sub DeepOwlButton_Click(sender As Object, e As EventArgs) Handles DeepOwlButton.Click
+        DisplayPictureBox.Image = DeepOwlPictureBox.Image
+        NameLabel.Text = "DeepOwl"
+        DifficultyLabel.Text = "Hard"
+        MovesLabel.Text = "Talon Swipe
+Feather Teleport
+Whirling Dash
+Flying Grab
+Flock of Feathers"
+        TypeOfEnemyLabel.Text = "Monster"
+    End Sub
+
+    Private Sub BrainSuckerButton_Click(sender As Object, e As EventArgs) Handles PrimadonButton.Click
+        DisplayPictureBox.Image = PrimadonPictureBox.Image
+        NameLabel.Text = "Primadon"
+        DifficultyLabel.Text = "Medium-Hard"
+        MovesLabel.Text = "Stomp
+Trample
+Punch
+Kick
+Grab
+Rock Throw
+Disengage
+Enrage"
+        TypeOfEnemyLabel.Text = "Boss Monster"
+    End Sub
+End Class
